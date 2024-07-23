af4dafbe25f2c52cddd7618de220cb2dafd9890e
maurosanchezv <minecraftultimatum39@gmail.com>
2024-07-23 17:47:31 -0400

    fix

--- projects/02-Tic-tac-toe/src/App.jsx
+++ projects/02-Tic-tac-toe/src/App.jsx
@@ -7,8 +7,17 @@ import { WinnerModal } from "./components/winnerModal.jsx";
 import "./App.css";
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem("board");
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null);
+  });
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ?? TURNS.X;
+  });
+
   //null Es que no hay ganador y false es que hay empate
   const [winner, setWinner] = useState(null);
 
@@ -16,6 +25,8 @@ function App() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+    window.localStorage.removeItem("board");
+    window.localStorage.removeItem("turn");
   };
 
   const checkEndGame = (newBoard) => {
@@ -32,6 +43,9 @@ function App() {
     //cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+    //Guardar aquí partida
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", newTurn);
     //Revisar si hay ganador
     const newWinner = checkWinner(newBoard);
     if (newWinner) {
